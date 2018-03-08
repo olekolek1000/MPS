92ea5fc445672c08697ecd1df0b6db5f87c5b3ae
Grzegorz Kociołek <georgekociolek@gmail.com>
2018-03-08 22:06:57 +0100

    Add files via upload

---
+++ modules/load_library.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifdef _WIN32
+    #include <windows.h>
+    typedef HINSTANCE libhandle_t
+#else
+    #include <dlfcn.h>
+    typedef void* libhandle_t;
+#endif
+
+static inline libhandle_t load_library(const char* path) {
+    libhandle_t libhandle_temp;
+    #ifdef _WIN32
+        libhandle_temp = LoadLibrary(path);
+    #else
+        libhandle_temp = dlopen(path,RTLD_LAZY);
+    #endif
+    return libhandle_temp;
+}
+
+static inline void* load_function(libhandle_t hand, const char* name) {
+    void* f;
+    #ifdef _WIN32
+        f = GetProcAddress(hand,name);
+    #else
+        f = dlsym(hand,name);
+    #endif
+    return f;
+}
+
+static inline int free_library(libhandle_t hand) {
+    #ifdef _WIN32
+        return FreeLibrary(hand);
+    #else
+        return dlclose(hand);
+    #endif
+}
+
