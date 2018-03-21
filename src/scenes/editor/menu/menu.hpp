971521a5079de2c9f76f2b3b185f36a90ec0a89a
olekolek1000 <olek_filip@wp.pl>
2018-03-21 17:01:56 +0100

    GIF support restored

--- src/scenes/editor/menu/menu.hpp
+++ src/scenes/editor/menu/menu.hpp
@@ -3,28 +3,13 @@
 #include "lib/glm.hpp"
 #include "texturemanager.hpp"
 #include "timestep.hpp"
-#include "../../../modules/module.hpp"
 
 class App;
 class sceneEditor;
 
-class pix_map {
-public:
-	int** pixmaps;
-	int* heights;
-	int* weights;
-
-	void Free();
-	void Malloc(size_t size);
-};
 
 class Menu{
 private:
-	pix_map current_export_data;
-
-	Module::Manager_t io_library_manager;
-	float current_export_thread_progress;
-
 	glm::mat4 projection, model;
 	void setProjection();
 	void exitMenu();
@@ -47,8 +32,6 @@ private:
 	void actionLoadProject();
 	void actionSaveProject();
 	void actionQuit();
-
-	bool LoadModule();
 	
 	Timestep step;
 public:
