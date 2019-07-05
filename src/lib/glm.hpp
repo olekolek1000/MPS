4ef7feaa2644ac0cdd6dd7551508635188968861
Devourer of Source <panczerwonyser@gmail.com>
2019-07-05 19:52:21 +0200

    Remove unnecessary system checks (#16)
    No need to check the system, android is commented and the rest is the same.

--- src/lib/glm.hpp
+++ src/lib/glm.hpp
@@ -1,23 +1,7 @@
 #pragma once
 
-#ifdef __linux__
-    //#define GLM_FORCE_RADIANS
-	#define GLM_ENABLE_EXPERIMENTAL
-    #include <glm/glm.hpp>
-    #include <glm/gtx/transform.hpp>
-#endif
-
-#ifdef __APPLE__
-	#define GLM_ENABLE_EXPERIMENTAL
-	#include <glm/glm.hpp>
-	#include <glm/gtx/transform.hpp>
-#endif
-
-#ifdef __MINGW32__
-    //#define GLM_FORCE_RADIANS
-	#define GLM_ENABLE_EXPERIMENTAL
-    #include <glm/glm.hpp>
-    #include <glm/gtx/transform.hpp>
-#endif
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 
 
--- src/lib/opengl.hpp
+++ src/lib/opengl.hpp
@@ -1,24 +1,9 @@
 #pragma once
 
-#ifdef __linux__
 #define GLEW_STATIC
 #include <GL/glew.h>
-//#include <SDL2/SDL_opengles2.h>
-#endif
 
 /*
 #ifdef __ANDROID__
 #include <SDL2/SDL_opengles2.h>
 #endif*/ 
-
-#ifdef __APPLE__
-#define GLEW_STATIC
-#include <GL/glew.h>
-#endif
-
-#ifdef __MINGW32__
-//#include <SDL2/SDL_opengles2.h>
-
-#define GLEW_STATIC
-#include <GL/glew.h>
-#endif
